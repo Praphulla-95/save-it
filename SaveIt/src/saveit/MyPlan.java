ffc28b5aed85ea3d1d6ad7559f8a3f4395228f6c
1895915 <1895915@E21043.CEGEPGIM.LCL>
2019-02-17 10:46:53 -0500

    Commit GetSet for MyPlan

---
+++ SaveIt/src/saveit/MyPlan.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package saveit;
+
+/**
+ *
+ * @author 1895915
+ */
+public class MyPlan {
+ 
+    String plan_name;
+    String plan_type;
+    
+    private void setplan_name(){
+        
+        this.plan_name=plan_name;
+        
+    }
+    private String getplan_name(){
+        
+        return plan_name;
+    }
+      private void setplan_type(){
+        
+        this.plan_type=plan_type;
+        
+    }
+    private String getplan_type(){
+        
+        return plan_type;
+    }
+}
+
+    
+
