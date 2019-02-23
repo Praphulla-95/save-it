7ddbd56aa5350e5d8df44b3db8469deec094e12c
Prashanth <prashanthbashetty@gmail.com>
2019-02-23 11:25:40 -0500

    Account class setter public commit

--- SaveIt/src/saveit/Account.java
+++ SaveIt/src/saveit/Account.java
@@ -13,29 +13,29 @@ public class Account {
     int account_No;
     String name;
     String card_Details;
-    private void setaccount_No(int account_No)
+    public void setaccount_No(int account_No)
     {
         this.account_No=account_No;
         
     }
-    private int getaccount_No()
+    public int getaccount_No()
     {
         return account_No;
         
     }
-    private void setname(String name)
+    public void setname(String name)
     {
         this.name=name;
     }
-    private String getname()
+    public String getname()
     {
         return name;
     }
-    private void setcard_Details(String card_Details)
+    public void setcard_Details(String card_Details)
     {
         this.card_Details=card_Details;
     }
-    private String getcard_Details()
+    public String getcard_Details()
     {
         return card_Details;
     }
