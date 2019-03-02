f301654e2b50c3f940261d9745e89a7b37da3aab
1894438 <1894438@E21042.CEGEPGIM.LCL>
2019-03-02 14:42:49 -0500

    category id create

--- SaveIt/src/saveit/Category.java
+++ SaveIt/src/saveit/Category.java
@@ -5,6 +5,9 @@
  */
 package saveit;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author 1894438
@@ -41,10 +44,48 @@ public class Category {
         return id;
     }
 
-    public void Category(String name, String description, String id){
+    public Category(Date newId,String name, String description, String id){
         
         this.name = name;
         this.description = description;
-        this.id = id;
+        this.id=generateid(newId, name);
+    }
+    public static String generateid(Date id,String fname)
+    {
+        
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MMM-dd");
+        String d=sdf.format(id);
+        String[] datesplit=d.split("-");
+        String formateddate=datesplit[0].substring(2);
+        formateddate =formateddate+"-" + datesplit[1];
+        if(Integer.parseInt(datesplit[2])%2==0)
+        {
+            formateddate += "-" + "E";
+        }
+        else
+        {
+            formateddate += "-" + "O";
+        }
+        if(fname.equals(""))
+        {
+            formateddate += "-" + "XX";
+        }
+        else if(fname.length()==1)
+        {
+            formateddate += "-" + fname.toUpperCase() + "X";
+        }
+        else if(fname.length()==2)
+        {
+            formateddate += "-" + fname.toUpperCase();
+        }
+        else
+        {
+            formateddate += "-" +fname.substring(0,1).toUpperCase()+fname.substring(fname.length()-1).toUpperCase();
+        }
+        return formateddate;
+    } 
+    public void display()
+    {
+        System.out.println("\n id is:"+ id +"\n name is:"+ name+"\n description is:"+description+"\n id is:"+id);
     }
 }
