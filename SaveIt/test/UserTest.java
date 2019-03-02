92327b86658ee14858df416fe4e83cd41c5fbe00
vikram.sumanth999@gmail.com <swaranbachu@gmail.com>
2019-03-02 15:41:24 -0500

    UserTest class unittest commit

---
+++ SaveIt/test/UserTest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author temp
+ */
+public class UserTest {
+    
+    public UserTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    @Test
+     public void generateIdTest()
+    {
+        Date date = new Date();
+        String name = "Lucas";
+        String expectedResult = "19-Mar-E-LS";
+        String realResult = saveit.Administrator.generateid(date, name);
+        assertEquals(expectedResult, realResult);
+    }
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+  }
