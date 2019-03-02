b8b9c4b704c2858949120d58fc6f44213486ffc9
1894438 <1894438@E21042.CEGEPGIM.LCL>
2019-03-02 16:24:06 -0500

    Wallet test commit

---
+++ SaveIt/test/WalletTest.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1894438
+ */
+public class WalletTest {
+    
+    public WalletTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+   @Test
+    public void generateIdTest()
+    {
+        System.out.println("Display Bill and Savings");
+        Date date = new Date();
+        String name = "XX";
+        String expectedResult = "19-Mar-E-XX";
+        String realResult = saveit.Wallet.generateid(date, name);
+        assertEquals(expectedResult, realResult);
+    }
+}
