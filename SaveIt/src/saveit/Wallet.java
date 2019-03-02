d61af32635cc0fbb49bb5ce172879d6fa97c2a37
1894438 <1894438@E21042.CEGEPGIM.LCL>
2019-03-02 14:44:00 -0500

    wallet id commit

--- SaveIt/src/saveit/Wallet.java
+++ SaveIt/src/saveit/Wallet.java
@@ -5,6 +5,9 @@
  */
 package saveit;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author 1894438
@@ -46,12 +49,50 @@ public class Wallet {
     public String getid(){
         return id;
     }
-    public void Wallet(Double bill, Double round_off_Amount, Double total_Amount, String id){
+    public Wallet(Date newId,Double bill, Double round_off_Amount, Double total_Amount, String id){
         
         this.bill = bill;
         this.round_off_Amount = round_off_Amount;
         this.total_Amount = total_Amount;
-        this.id = id;
+        this.id=generateid(newId,"");
+    }
+     public static String generateid(Date id,String fname)
+    {
+        
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MMM-dd");
+        String d=sdf.format(id);
+        String[] datesplit=d.split("-");
+        String formateddate=datesplit[0].substring(2);
+        formateddate =formateddate+"-" + datesplit[1];
+        if(Integer.parseInt(datesplit[2])%2==0)
+        {
+            formateddate += "-" + "E";
+        }
+        else
+        {
+            formateddate += "-" + "O";
+        }
+        if(fname.equals(""))
+        {
+            formateddate += "-" + "XX";
+        }
+        else if(fname.length()==1)
+        {
+            formateddate += "-" + fname.toUpperCase() + "X";
+        }
+        else if(fname.length()==2)
+        {
+            formateddate += "-" + fname.toUpperCase();
+        }
+        else
+        {
+            formateddate += "-" +fname.substring(0,1).toUpperCase()+fname.substring(fname.length()-1).toUpperCase();
+        }
+        return formateddate;
+    } 
+    public void display()
+    {
+        System.out.println("\n id is:"+ id +"\n bill is:"+ bill+"\n round_of_amount is:"+round_off_Amount+"\n total_Amount is:"+total_Amount+"\n id is:"+id);
     }
     Store s = new Store();
 }
