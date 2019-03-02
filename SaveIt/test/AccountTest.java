5e28c188b79134b32d7ac65926d21695382a04fa
1895915 <1895915@E21043.CEGEPGIM.LCL>
2019-03-02 16:34:12 -0500

    TransfersTest class commit

---
+++ SaveIt/test/AccountTest.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895915
+ */
+public class AccountTest {
+    
+    public AccountTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+     @Test
+    public void generateIdTest()
+    {
+        Date date = new Date();
+        String name = "Teja";
+        String expectedResult = "19-Mar-E-TA";
+        String realResult = saveit.Account.generateid(date, name);
+        assertEquals(expectedResult, realResult);
+    }
+}
